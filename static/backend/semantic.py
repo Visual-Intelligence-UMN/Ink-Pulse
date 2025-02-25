887639fb69e6144cd5d2d5a9ea2834e06fd364af
Gayathri Balaji <gayathri.balaji.1998@gmail.com>
2025-02-25 13:11:45 -0600

    Changes for cosine similarity semantics

--- static/backend/semantic.py
+++ static/backend/semantic.py
@@ -26,7 +26,7 @@ def sentence_similarity(sent1, sent2, model):
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))  
     static_dir = os.path.dirname(script_dir)
-    session_dir = os.path.join(static_dir, "chi2022-coauthor-v1.0\coauthor-sentence")
+    session_dir = os.path.join(static_dir, "chi2022-coauthor-v1.0/coauthor-sentence")
     session_files = [f for f in os.listdir(session_dir) if f.endswith(".jsonl")]
     for session in session_files:
         session_path = os.path.join(session_dir, session)
