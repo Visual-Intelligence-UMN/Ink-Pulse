0b800dad2908feec8a49041a141176f5519a38da
Ether9t <wan01492@umn.edu>
2025-04-23 20:41:00 -0500

    Fix switch issue

--- static/backend/height_calculate.py
+++ static/backend/height_calculate.py
@@ -8,20 +8,26 @@ def read_file(file_path):
     if not isinstance(data, list) or len(data) == 0 or not all(isinstance(item, dict) for item in data):
         print(f"Invalid structure in file {file_path}")
         return
-
-    for i, event in enumerate(data):
-        if i == 0:
-            event["height"] = event["end_progress"] - event["start_progress"]
-            event["norm_vector"] = 0
-        else:
-            height = event["end_progress"] - event["start_progress"]
-            if height < 0:
-                height = 0
-            event["height"] = height
-            event["norm_vector"] = event["residual_vector_norm"] / len(event["sentence"])
-    max_norm_vector = max(event["norm_vector"] for event in data)
+    
     for event in data:
-        event["max_norm_vector"] = max_norm_vector
+        if event["start_progress"] > event["end_progress"]:
+            t = event["start_progress"]
+            event["start_progress"] = event["end_progress"]
+            event["end_progress"] = t
+
+    # for i, event in enumerate(data):
+    #     if i == 0:
+    #         event["height"] = event["end_progress"] - event["start_progress"]
+    #         event["norm_vector"] = 0
+    #     else:
+    #         height = event["end_progress"] - event["start_progress"]
+    #         if height < 0:
+    #             height = 0
+    #         event["height"] = height
+    #         event["norm_vector"] = event["residual_vector_norm"] / len(event["sentence"])
+    # max_norm_vector = max(event["norm_vector"] for event in data)
+    # for event in data:
+    #     event["max_norm_vector"] = max_norm_vector
 
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False,  indent=4)
