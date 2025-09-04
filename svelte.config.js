c247a3567a4ecaa3303b92de4c4b9843dc5b45b1
Ether9t <wan01492@umn.edu>
2025-09-04 14:44:49 -0500

    Test /doc

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,17 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/Ink-Pulse',
-			relative: false
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/Ink-Pulse',
+      relative: false
+    }
+  }
 };
 
 export default config;
